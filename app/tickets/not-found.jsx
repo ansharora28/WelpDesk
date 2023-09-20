d500b9e05b538bdccd8403e77d85e1d4aaca79ca
Ansh <ansharorake2@gmail.com>
2023-09-20 15:34:38 +0530

    feat: custom 404

---
+++ app/tickets/not-found.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export default function NotFound() {
+  return (
+    <main className="text-center">
+        <h2 className="text-3xl">We hit a brick wall!</h2>
+        <p>We could not find the ticket you were looking for.</p>
+        <p>Go back to <Link href="/tickets">Tickets</Link></p>
+    </main>
+  )
+}
