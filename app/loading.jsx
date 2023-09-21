cf86236e0ca75a528f5e7df1e995915c0a64e909
Ansh <ansharorake2@gmail.com>
2023-09-22 01:26:23 +0530

    feat: add loading component and suspend UI

---
+++ app/loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <main className="text-center">
+        <h2 className="text-primary">Loading Tickets...</h2>
+        <p>Hold tight!</p>
+    </main>
+  )
+}
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import TicketList from './TicketList'
+import Loading from '../loading'
 
 export default function Tickets() {
   return (
@@ -8,9 +9,11 @@ export default function Tickets() {
         <div>
           <h2>Tickets</h2>
           <p><small>Currently Open Tickets</small></p>
-          <TicketList />
         </div>
       </nav>
+      <Suspense fallback= { <Loading />}>
+          <TicketList />
+      </Suspense>
     </main>
   )
 }
