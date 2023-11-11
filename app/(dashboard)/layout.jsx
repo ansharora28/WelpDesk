668a788f5aa3048007ab57ba8ec494c94b9ac80f
Ansh <ansharorake2@gmail.com>
2023-11-12 02:52:50 +0530

    set up logout (display user is not working - data.session.user is null)

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
 // components
 import Navbar from '@/app/components/Navbar'
-export default function DashBoardLayout({children}) {
+
+export default async function DashboardLayout({ children }) {
+  const supabase = createServerComponentClient({ cookies })
+  const { data } = await supabase.auth.getSession()
+
   return (
     <>
-    <Navbar />
-    {children}
+      <Navbar  />
+      {children}
     </>
   )
 }
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import Logo from './logo.png'
-export default function Navbar() {
+import LogoutButton from './LogoutButton'
+
+export default function Navbar({ user }) {
   return (
     <nav>
-        <Image
-          src= {Logo}
-          alt = "WelpDesk Logo"
-          width = {70}
-          quality = {100}
-          placeholder = "blur"
-        />
-        <h1>WelpDesk</h1>
-        <Link href="/">Dashboard</Link>  
-        <Link href="/tickets">Tickets</Link>  
+      <Image
+        src={Logo}
+        alt='Dojo Helpdesk logo'
+        width={70}
+        placeholder='blur'
+        quality={100}
+      />
+      <h1>Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets" className="mr-auto">Tickets</Link>
+      {user && <span>Hello, {user.email}</span>}
+      <LogoutButton />
     </nav>
   )
 }
