5871de54ffc4d0c22aa037fbf9eca46ab34d7d5e
Ansh <ansharorake2@gmail.com>
2023-11-29 15:25:38 +0530

    route protection

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,13 +1,24 @@
 import Link from "next/link"
-export default function AuthLayout({children}) {
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+
+export default async function AuthLayout({ children }) {
+  const supabase = createServerComponentClient({ cookies })
+  const { data } = await supabase.auth.getSession()
+
+  if (data.session) {
+    redirect('/')
+  }
+
   return (
     <>
-    <nav>
-        <h1>WelpDesk</h1>
+      <nav>
+        <h1>Dojo Helpdesk</h1>
+        <Link href="/signup">Sign up</Link>
         <Link href="/login">Login</Link>
-        <Link href="/signup">Signup</Link>
-    </nav>
-    {children}
+      </nav>
+      {children}
     </>
   )
 }
