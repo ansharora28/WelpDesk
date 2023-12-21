0d5b3e89c3ea0a848b7c65a5c9f3afb677f6dd44
Ansh <ansharorake2@gmail.com>
2023-12-21 23:46:33 +0530

    setup db

--- app/(dashboard)/tickets/create/CreateForm.jsx
+++ app/(dashboard)/tickets/create/CreateForm.jsx
@@ -15,19 +15,14 @@ export default function CreateForm() {
     e.preventDefault()
     setIsLoading(true)
 
-    const newTicket = { title, body, priority, user_email: 'abc@g.com' }
+    const newTicket = { title, body, priority }
 
-    const res = await fetch('http://localhost:4000/tickets', {
+    const res = await fetch('http://localhost:3000/api/tickets', {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify(newTicket)
     })
 
-    if (res.status === 201) {
-      router.refresh()
-      router.push('/tickets')
-    }
-    
   }
 
   return (
--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -2,16 +2,6 @@ import { NextResponse } from "next/server"
 
 export const dynamic = 'force-dynamic'
 
-export async function GET() {
-  const res = await fetch('http://localhost:4000/tickets')
-
-  const tickets = await res.json()
-
-  return NextResponse.json(tickets, {
-    status: 200
-  })
-}
-
 export async function POST(request) {
   const ticket = await request.json()
 
